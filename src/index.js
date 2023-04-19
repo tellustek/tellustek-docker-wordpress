19961e92c0b1ef02646df76265d37fab7d45b4d6
Shingo <shingo0620@gmail.com>
2023-04-19 11:33:36 +0800

    Check server status before start the browser sync server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-require('dotenv').config()
-const browserSync = require('browser-sync').create()
+import got from 'got'
+import dotenv from 'dotenv'
+import browserSync from 'browser-sync'
+import { spawn } from 'child_process'
 
-browserSync.init({
-  ui: false,
+dotenv.config()
+let postStartProcess
+const bsServer = browserSync.create()
 
-  files: [
-    './src/wordpress/themes/**/*.html',
-    './src/wordpress/themes/**/*.css',
-    './src/wordpress/themes/**/*.php',
-    './src/wordpress/themes/**/*.js'
-  ],
+let timer = setInterval(async () => {
+  try {
+    console.log('Check server status...')
+    const res = await got.get('http://127.0.0.1:1337')
+    console.log('complete')
+    clearInterval(timer)
+    browserSync.init({
+      ui: false,
 
-  proxy: `${process.env.DEV_HOST || 'http://127.0.0.1'}:${process.env.DEV_WEB_PORT || '1337'}`
+      watchOptions: {
+        ignoreInitial: true,
+        ignored: 'node_modules'
+      },
+
+      ignore: [
+        '**/node_modules/**/*',
+        './src/wordpress/themes/twentytwentyone/**/*',
+        './src/wordpress/themes/twentytwentytwo/**/*',
+        './src/wordpress/themes/twentytwentythree/**/*'
+      ],
+
+      files: [
+        './src/wordpress/themes/**/*.html',
+        './src/wordpress/themes/**/*.css',
+        './src/wordpress/themes/**/*.php',
+        './src/wordpress/themes/**/*.js'
+      ],
+
+      proxy: `${process.env.DEV_HOST || 'http://127.0.0.1'}:${process.env.DEV_WEB_PORT || '1337'}`
+    })
+  } catch (err) {
+    console.log('check fail...')
+  }
+}, 5000)
+
+process.on('SIGINT', () => {
+  console.log('Browsersync關閉中...如需停止 docker container 請執行 npm run stop')
+  process.exit(0);
 })
